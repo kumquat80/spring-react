fcedabb5621c99f96986c6111dff5fa5df051324
Maciej Kapuscinski <maciej.kapuscinski@tedd.berlin>
2018-10-11 21:50:08 +0200

    initial project structure

---
+++ src/test/kotlin/pl/kumquat/webapp/springreact/SpringReactApplicationTests.kt
@@ -0,0 +1,16 @@
+package pl.kumquat.webapp.springreact
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringReactApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
